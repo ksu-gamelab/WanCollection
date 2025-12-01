582f9e119bc4f596ecf8b646f489a5f7ba02b50f
nkhrktm <125846541+nkhrktm@users.noreply.github.com>
2025-12-01 21:45:59 +0900

    コーギー追加

---
+++ WanCollection/Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSelect : MonoBehaviour
+{
+
+
+    [SerializeField] private string sceneName; // Å© Inspector Ç≈ì¸óÕÇ≈Ç´ÇÈÅI
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
